73a31d931a91a80d7fa78d88bb168085f661f32e
Allen Kramer <kramer.alleng@gmail.com>
2016-12-29 15:51:13 -0500

    added posts controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,81 @@
+class QuestionsController < ApplicationController
+  def index
+    
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  # def index
+  #   @posts = Post.all
+  # end
+  #
+  # def show
+  #   @post = Post.find(params[:id])
+  # end
+  #
+  # def new
+  #   @post = Post.new
+  # end
+  #
+  # def create
+  #   @post = Post.new
+  #   @post.title = params[:post][:title]
+  #   @post.body = params[:post][:body]
+  #
+  #   if @post.save
+  #     flash[:notice] = "Post saved successfully"
+  #     redirect_to @post
+  #   else
+  #     flash.now[:alert] = "There was an error saving the post.  Please try again."
+  #     render :new
+  #   end
+  # end
+  #
+  # def edit
+  #   @post = Post.find(params[:id])
+  # end
+  #
+  # def update
+  #   @post = Post.find(params[:id])
+  #   @post.title = params[:post][:title]
+  #   @post.body = params[:post][:body]
+  #
+  #   if @post.save
+  #     flash[:notice] = "Post was updated successfully"
+  #     redirect_to @post
+  #   else
+  #     flash.now[:alert] = "There was an error updating the post.  Please try again."
+  #     render :edit
+  #   end
+  # end
+  #
+  # def destroy
+  #   @post = Post.find(params[:id])
+  #
+  #   if @post.destroy
+  #     flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+  #     redirect_to posts_path
+  #   else
+  #     flash.now[:alert] = "There was an error deleting the post.  Please try again."
+  #     render :show
+  #   end
+  # end
+end
